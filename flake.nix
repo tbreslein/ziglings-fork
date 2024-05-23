e835a8593ccc87c62665a647d09d1540e7258135
Tommy Breslein <tommy.breslein@protonmail.com>
2024-05-23 16:53:56 +0200

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "ziglings solutions";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {buildInputs = with pkgs; [zig zls nil statix alejandra];};
+    });
+}
